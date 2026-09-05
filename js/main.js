773469ee80b7e0232d12375d34bfa08c56ce4771
Tina Ayanda Fezani <fezaniayanda@gmail.com>
2026-09-05 16:45:41 +0200

    refined collaboration title

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* =========================================
+   KFC × BOYZNBUCK$
+
+   PAGE 01 — COVER
+========================================= */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("KFC × BOYZNBUCK$ — Page 01 loaded.");
+
+});
